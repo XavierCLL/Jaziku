5dc9b3cfb4616a76b84244ed72c6fb6096be55a7
Xavier Corredor <xcorredorl@ideam.gov.co>
2012-11-21 21:39:13 -0500

    - Now use NCARG_ROOT variable defined into system for locate the ncarg libraries - Now check the basic requirements for maps (ncl, convert, and others)

--- jaziku/utils/console.py
+++ jaziku/utils/console.py
@@ -164,3 +164,24 @@ class redirectStdStreams(object):
         self._stdout.flush(); self._stderr.flush()
         sys.stdout = self.old_stdout
         sys.stderr = self.old_stderr
+
+
+def which(program):
+    """
+    Test if external program exists and return path
+    """
+    import os
+    def is_exe(fpath):
+        return os.path.isfile(fpath) and os.access(fpath, os.X_OK)
+
+    fpath, fname = os.path.split(program)
+    if fpath:
+        if is_exe(program):
+            return program
+    else:
+        for path in os.environ["PATH"].split(os.pathsep):
+            exe_file = os.path.join(path, program)
+            if is_exe(exe_file):
+                return exe_file
+
+    return None
