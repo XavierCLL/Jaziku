39f0d19108b267c0847a0cb6f451d49e704b39b7
Xavier Corredor <xcorredorl@ideam.gov.co>
2013-01-14 13:07:22 -0500

    - Updated setup.py file

--- setup.py
+++ setup.py
@@ -1,53 +1,48 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+# Copyright © 2011-2013 IDEAM
+#
+# This file is part of Jaziku.
+#
+# Jaziku is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Jaziku is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Jaziku.  If not, see <http://www.gnu.org/licenses/>.
+
 from setuptools import setup, find_packages
 from jaziku.utils import globals_vars
 
 name = globals_vars.PROG_NAME
 
-setup(name=globals_vars.PROG_NAME,
-      version=globals_vars.VERSION,
-      license="GNU General Public License (GPL) v3",
-      description="""Jaziku is a software for the implementation of composite analysis
-                  methodology between the major indices of climate variability and major
-                  meteorological variables in puntual scale.""",
-      long_description="""
-        Jaziku is a software for the implementation of composite analysis
-        methodology between the major indices of climate variability and major
-        meteorological variables in puntual scale.
-
-        According to IDEAM’s commitment to promote and guide scientiﬁc research
-        about weather forecasting and climate, "Jazikü" is a program designed to
-        evaluate teleconnections between meteorological variables with the main
-        indices of climate variability aﬀecting climate in Colombia.
-
-        Jaziku, follows the composite methodology analysis proposed by The
-        University Corporation for Atmospheric Research (UCAR)), National Oceanic
-        and Atmospheric Administration (NOAA) & U.S. Department of Commerce
-        (DOC)[1][2][3] and can produce probability scenarios
-        under which it is expected precipitation or any other variable for speciﬁc
-        sites or areas interpolated to behave, as a function of the probability
-        values predicted for each climate variability and the history of involvement
-        in the quarterly average level. Such scenarios become a powerful tool for
-        decision making by the national meteorological services
-
-        [1] National Oceanic and Atmospheric Administration (NOAA) , University
-        Corporation for Atmospheric Research (UCAR)). Creating a Local Climate
-        Product Using Composite Analysis - Print Version of Webcast -(En Linea).
-        1997-2010:COMET Website at http://meted.ucar.edu/, 1997.
-        [2] A. Leetmaa Barnston, A. G. NCEP Forecasts of the El Niño of 1997 1998
-        and Its U.S. Impacts. Bull. Amer. Met. Soc, 80:1829 – 1852, 1999.
-        [3] M. B. Richman Montroy, D.L. Observed Nonlinearities of Monthly
-        Teleconnections between Tropical Paciﬁc Sea Surface Temperature Anomalies
-        and Central and Eastern North American Precipitation. Journal of Climate,
-        11:1812 – 1835, 1998.""",
-      author="Ines Sánchez Rodriguez, Xavier Corredor Llano",
-      author_email="incsanchezro (a) gmail.com, xcorredorl (a) ideam.gov.co",
-      url="http://hg.ideam.gov.co:8000/meteorologia/jaziku",
-      platforms="platform-independent",
-      install_requires=["scipy", "argparse", "python-dateutil", "matplotlib", "numpy", "PIL", "clint"],
-      scripts=["bin/jaziku"],
-      packages=find_packages(),
-      include_package_data=True,
+setup(
+    name=globals_vars.PROG_NAME,
+    version=globals_vars.VERSION,
+    license="GNU General Public License (GPL) v3",
+    description="Jaziku is statistical inference software for the teleconnections analysis.",
+    long_description=open('README.rst').read(),
+    classifiers=[
+        'Environment :: Console',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python',
+        'Topic :: Scientific/Engineering',
+        'Topic :: Scientific/Engineering :: Atmospheric Science'],
+    author="Xavier Corredor Llano, Ines Sánchez Rodriguez",
+    author_email="xcorredorl (a) ideam.gov.co, incsanchezro (a) gmail.com",
+    url="http://hg.ideam.gov.co:8000/meteorologia/jaziku",
+    platforms="platform-independent",
+    install_requires=["scipy", "argparse", "python-dateutil", "matplotlib", "numpy", "PIL", "clint"],
+    scripts=["bin/jaziku"],
+    packages=find_packages(),
+    include_package_data=True,
 )
