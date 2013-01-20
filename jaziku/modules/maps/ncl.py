b22b3fddd60c1c2b4f632341dc03224a8e0d79bd
Xavier Corredor Llano <xcorredorl@ideam.gov.co>
2013-01-20 18:52:53 -0500

    - Fix missing import - Fix string spanish translation (transplanted from 21b805ae71373d3c9410a131edcb7fc88db3ace7)

--- jaziku/modules/maps/ncl.py
+++ jaziku/modules/maps/ncl.py
@@ -23,6 +23,8 @@ import imp
 
 from jaziku.env import config_run
 
+from jaziku.utils import globals_vars
+
 
 class MapProperties(object):
     pass
@@ -91,7 +93,7 @@ def make_ncl_file(grid, base_path_file, globals_vars):
             if grid.need_particular_ncl_script:
                 ncl_script = imp.load_source("ncl_script", os.path.join(grid.shape_path, "ncl.py"))
             else:
-                ncl_script = imp.load_source("ncl_script", os.path.join(globals_vars.ROOT_DIR, 'data', 'maps', 'shapes', "ncl.py"))
+                ncl_script = imp.load_source("ncl_script", os.path.join(globals_vars.JAZIKU_DIR, 'data', 'maps', 'shapes', "ncl.py"))
 
         ncl_file_raw = ncl_script.code(map_properties)
 
