5c1773d9440820b6c456992258fa163773b1bc78
Xavier Corredor Llano <xcorredorl@ideam.gov.co>
2013-02-11 22:24:17 -0500

    - Fixes and adjust console text message mainly in prepare data of all stations

--- jaziku/core/stations.py
+++ jaziku/core/stations.py
@@ -33,52 +33,51 @@ def prepare_all_stations(stations_list):
             "################# PREPARE DATA OF ALL STATIONS #################\n")
 
     # Read vars
-    console.msg(_("\nReading var D and var I of all stations .............. "), newline=False)
+    console.msg(_("Reading var D and var I of all stations ................. "), newline=False)
     for station in stations_list:
         station.var_D.read_data_from_file(station, process=True, messages=False)
         station.var_I.read_data_from_file(station, process=True, messages=False)
     console.msg(_("done"), color='green')
 
     # common and process period
-    console.msg(_("\nCalculate common and process period for each stations ... "), newline=False)
+    console.msg(_("Calculate common and process period for each stations ... "), newline=False)
     for station in stations_list:
         station.calculate_common_and_process_period()
     console.msg(_("done"), color='green')
 
     # data, date and null
-    console.msg(_("\Prepare data, date and null in process period ......... "), newline=False)
+    console.msg(_("Prepare data, date and null in process period ........... "), newline=False)
     for station in stations_list:
         station.var_D.data_and_null_in_process_period(station)
         station.var_I.data_and_null_in_process_period(station)
     console.msg(_("done"), color='green')
 
     if config_run.settings['consistent_data']:
-        console.msg(_("Check if the data are consistent for var D and I"))
+        console.msg(_("Check if the data are consistent for var D and I:"))
         for station in stations_list:
             input_check.check_consistent_data(station)
 
     # statistics for data analysis
     if config_run.settings['data_analysis']:
-        console.msg(_("\nStatistics of data for data analysis module ............... "), newline=False)
+        console.msg(_("Statistics of data for data analysis module ............. "), newline=False)
         for station in stations_list:
             station.var_D.do_some_statistic_of_data()
             station.var_I.do_some_statistic_of_data()
         console.msg(_("done"), color='green')
 
     # state of data
-    console.msg(_("\nSet global state of data ............................. "), newline=False)
+    console.msg(_("Set global state of data ................................ "), newline=False)
     analysis_interval.set_global_state_of_data(stations_list)
     console.msg(_("done"), color='green')
 
     # analysis interval
-    console.msg(_("\nCheck analysis interval ............................. "), newline=False)
+    console.msg(_("Check analysis interval ................................. "), newline=False)
     analysis_interval.check_analysis_interval()
     console.msg(_("done"), color='green')
 
-
-    console.msg(_("done"), color='green')
+    console.msg('')
     console.msg(gettext.ngettext(
-        _("   {0} station prepared."),
-        _("   {0} stations prepared."),
+        _("{0} station prepared."),
+        _("{0} stations prepared."),
         Station.stations_processed).format(Station.stations_processed), color='cyan')
     console.msg('')
