4cd5ac2421d8e42d3e6132db5613f26636e243c6
Xavier Corredor Llano <xcorredorl@ideam.gov.co>
2013-02-13 18:02:16 -0500

    Jaziku 0.5.2a

--- jaziku/utils/globals_vars.py
+++ jaziku/utils/globals_vars.py
@@ -29,9 +29,9 @@ from math import isnan
 
 PROG_NAME = "jaziku"
 
-VERSION = "0.5.2"
+VERSION = "0.5.2a"
 
-VERSION_DATE = "24/01/2013"
+VERSION_DATE = "13/02/2013"
 
 # absolute directory where is installed Jaziku in your system,
 # this variable is set in jaziku.py
