1c14eb48b17948607a20614143de7f865c7846e7
Xavier Corredor Llano <xcorredorl@ideam.gov.co>
2013-02-26 13:14:16 -0500

    - partial updated contingency_table

--- jaziku/modules/climate/contingency_table.py
+++ jaziku/modules/climate/contingency_table.py
@@ -22,48 +22,93 @@ import sys
 import os
 from numpy import matrix
 
-from jaziku.env import globals_vars, config_run
+from jaziku import env
 from jaziku.utils import format_out
 from jaziku.utils import console
 from jaziku.modules.climate import lags
-from jaziku.modules.climate.thresholds import get_thresholds_var_D, get_thresholds_var_I
+from jaziku.modules.climate.thresholds import get_thresholds
 
 
-def get_category_of_phenomenon(value, station):
+def get_label_of_var_I_category(value, station):
     """
-    Calculate the category of phenomenon for independent variable based on label phenomenon,
-    evaluate where is the 'value' in the thresholds (by default or defined by user), this is
-    below, normal or above and return the label for this position.
+    Calculate, for a particular 'value', the var I category label based on 'var_I_category_labels'
+    and thresholds of var I, evaluate where is the 'value' inside the thresholds and return
+    the correspondent label for this position.
 
-    For some variables for set the category of phenomenon the thresholds are inclusive,
+    For some variables, for set a particular var I category the thresholds are inclusive,
     for ('ONI1', 'ONI2', 'W850w', 'SST4', 'SST12', 'ASST3', 'ASST34', 'ASST4', 'ASST12'):
         the value is not normal when { threshold below >= value >= threshold above }
     else:
         the value is not normal when { threshold below > value > threshold above }
+
+    :param value: values for calculate the label
+    :type value: float
+    :param station: station of value
+    :type station: Station
+
+    :return: label
+    :rtype: str
     """
 
     # get thresholds of var I in the period of outlier
-    threshold_below_var_I, threshold_above_var_I = get_thresholds_var_I(station)
+    thresholds_var_I = get_thresholds(station, station.var_I)
 
-    # categorize the value of var I and get the category_of_phenomenon based in the label phenomenon
+    # categorize the value of var I and get the label_of_var_I_category based in the label phenomenon
 
     # SPECIAL CASE 2: for some variables for set the category of phenomenon the thresholds are inclusive
-    if station.var_I.type_series in ['ONI1', 'ONI2', 'SST12', 'SST3', 'SST4', 'SST34', 'ASST12', 'ASST3', 'ASST4', 'ASST34']:
-        if value <= threshold_below_var_I:
-            category_of_phenomenon = globals_vars.phenomenon_below
-        elif value >= threshold_above_var_I:
-            category_of_phenomenon = globals_vars.phenomenon_above
-        else:
-            category_of_phenomenon = globals_vars.phenomenon_normal
-    else:
-        if value < threshold_below_var_I:
-            category_of_phenomenon = globals_vars.phenomenon_below
-        elif value > threshold_above_var_I:
-            category_of_phenomenon = globals_vars.phenomenon_above
-        else:
-            category_of_phenomenon = globals_vars.phenomenon_normal
+    if env.config_run.settings['type_var_I'] in ['ONI1', 'ONI2', 'SST12', 'SST3', 'SST4', 'SST34', 'ASST12', 'ASST3', 'ASST4', 'ASST34']:
 
-    return category_of_phenomenon
+        if env.config_run.settings['class_category_analysis'] == 3:
+            if value <= thresholds_var_I['below']:
+                label_of_var_I_category = env.config_run.settings['var_I_category_labels']['below']
+            elif value >= thresholds_var_I['above']:
+                label_of_var_I_category = env.config_run.settings['var_I_category_labels']['above']
+            else:
+                label_of_var_I_category = env.config_run.settings['var_I_category_labels']['normal']
+
+        if env.config_run.settings['class_category_analysis'] == 7:
+            if value <= thresholds_var_I['below3']:
+                label_of_var_I_category = env.config_run.settings['var_I_category_labels']['below3']
+            elif thresholds_var_I['below3'] < value <= thresholds_var_I['below2']:
+                label_of_var_I_category = env.config_run.settings['var_I_category_labels']['below2']
+            elif thresholds_var_I['below2'] < value <= thresholds_var_I['below1']:
+                label_of_var_I_category = env.config_run.settings['var_I_category_labels']['below1']
+            elif thresholds_var_I['below1'] < value < thresholds_var_I['above1']:
+                label_of_var_I_category = env.config_run.settings['var_I_category_labels']['normal']
+            elif thresholds_var_I['above1'] <= value < thresholds_var_I['above2']:
+                label_of_var_I_category = env.config_run.settings['var_I_category_labels']['above1']
+            elif thresholds_var_I['above2'] <= value < thresholds_var_I['above3']:
+                label_of_var_I_category = env.config_run.settings['var_I_category_labels']['above2']
+            elif value >= thresholds_var_I['above3']:
+                label_of_var_I_category = env.config_run.settings['var_I_category_labels']['above3']
+    else:
+
+        if env.config_run.settings['class_category_analysis'] == 3:
+            if value < thresholds_var_I['below']:
+                label_of_var_I_category = env.config_run.settings['var_I_category_labels']['below']
+            elif value > thresholds_var_I['above']:
+                label_of_var_I_category = env.config_run.settings['var_I_category_labels']['above']
+            else:
+                label_of_var_I_category = env.config_run.settings['var_I_category_labels']['normal']
+
+        if env.config_run.settings['class_category_analysis'] == 7:
+            if value < thresholds_var_I['below3']:
+                label_of_var_I_category = env.config_run.settings['var_I_category_labels']['below3']
+            elif thresholds_var_I['below3'] <= value < thresholds_var_I['below2']:
+                label_of_var_I_category = env.config_run.settings['var_I_category_labels']['below2']
+            elif thresholds_var_I['below2'] <= value < thresholds_var_I['below1']:
+                label_of_var_I_category = env.config_run.settings['var_I_category_labels']['below1']
+            elif thresholds_var_I['below1'] <= value <= thresholds_var_I['above1']:
+                label_of_var_I_category = env.config_run.settings['var_I_category_labels']['normal']
+            elif thresholds_var_I['above1'] < value <= thresholds_var_I['above2']:
+                label_of_var_I_category = env.config_run.settings['var_I_category_labels']['above1']
+            elif thresholds_var_I['above2'] < value <= thresholds_var_I['above3']:
+                label_of_var_I_category = env.config_run.settings['var_I_category_labels']['above2']
+            elif value > thresholds_var_I['above3']:
+                label_of_var_I_category = env.config_run.settings['var_I_category_labels']['above3']
+
+
+    return label_of_var_I_category
 
 
 def get_contingency_table(station, lag, month, day=None):
@@ -75,23 +120,28 @@ def get_contingency_table(station, lag, month, day=None):
 
     if day is None:
         # get all values of var D and var I based on this lag and month
-        station.var_D_values = lags.get_lag_values(station, 'var_D', lag, month)
-        station.var_I_values = lags.get_lag_values(station, 'var_I', lag, month)
+        station.var_D.specific_values = lags.get_lag_values(station, 'var_D', lag, month)
+        station.var_I.specific_values = lags.get_lag_values(station, 'var_I', lag, month)
 
     if day is not None:
         # get all values of var D and var I based on this lag and month
-        station.var_D_values = lags.get_lag_values(station, 'var_D', lag, month, day)
-        station.var_I_values = lags.get_lag_values(station, 'var_I', lag, month, day)
+        station.var_D.specific_values = lags.get_lag_values(station, 'var_D', lag, month, day)
+        station.var_I.specific_values = lags.get_lag_values(station, 'var_I', lag, month, day)
 
     # calculate thresholds as defined by the user in station file for var D
-    threshold_below_var_D, threshold_above_var_D = get_thresholds_var_D(station)
+    thresholds_var_D = get_thresholds(station, station.var_D)
 
     # calculate thresholds as defined by the user in station file for var I
-    threshold_below_var_I, threshold_above_var_I = get_thresholds_var_I(station)
+    thresholds_var_I = get_thresholds(station, station.var_I)
+
+    print station.var_D.specific_values
+    print thresholds_var_D
+    print thresholds_var_I
+    exit()
 
     # this is to print later in contingency table
-    thresholds_var_D_var_I = [format_out.number(threshold_below_var_D), format_out.number(threshold_above_var_D),
-                              format_out.number(threshold_below_var_I), format_out.number(threshold_above_var_I)]
+    thresholds_var_D_var_I = [format_out.number(thresholds_var_D['below']), format_out.number(thresholds_var_D['above']),
+                              format_out.number(thresholds_var_I['below']), format_out.number(thresholds_var_I['above'])]
 
     # -------------------------------------------------------------------------
     ## Calculating contingency table with absolute values
@@ -99,52 +149,52 @@ def get_contingency_table(station, lag, month, day=None):
     contingency_table = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
 
     # SPECIAL CASE 2: for some variables for set the category of phenomenon the thresholds are inclusive (<=, >=)
-    if station.var_I.type_series in ['ONI1', 'ONI2', 'SST12', 'SST3', 'SST4', 'SST34', 'ASST12', 'ASST3', 'ASST4', 'ASST34']:
-        for index, var_I in enumerate(station.var_I_values):
-            if var_I <= threshold_below_var_I:
-                if station.var_D_values[index] <= threshold_below_var_D:
+    if env.config_run.settings['type_var_I'] in ['ONI1', 'ONI2', 'SST12', 'SST3', 'SST4', 'SST34', 'ASST12', 'ASST3', 'ASST4', 'ASST34']:
+        for index, var_I in enumerate(station.var_I.specific_values):
+            if var_I <= thresholds_var_I['below']:
+                if station.var_D.specific_values[index] <= thresholds_var_D['below']:
                     contingency_table[0][0] += 1
-                if threshold_below_var_D < station.var_D_values[index] < threshold_above_var_D:
+                if thresholds_var_D['below'] < station.var_D.specific_values[index] < thresholds_var_D['above']:
                     contingency_table[0][1] += 1
-                if station.var_D_values[index] >= threshold_above_var_D:
+                if station.var_D.specific_values[index] >= thresholds_var_D['above']:
                     contingency_table[0][2] += 1
-            if threshold_below_var_I < var_I < threshold_above_var_I:
-                if station.var_D_values[index] <= threshold_below_var_D:
+            if thresholds_var_I['below'] < var_I < thresholds_var_I['above']:
+                if station.var_D.specific_values[index] <= thresholds_var_D['below']:
                     contingency_table[1][0] += 1
-                if threshold_below_var_D < station.var_D_values[index] < threshold_above_var_D:
+                if thresholds_var_D['below'] < station.var_D.specific_values[index] < thresholds_var_D['above']:
                     contingency_table[1][1] += 1
-                if station.var_D_values[index] >= threshold_above_var_D:
+                if station.var_D.specific_values[index] >= thresholds_var_D['above']:
                     contingency_table[1][2] += 1
-            if var_I >= threshold_above_var_I:
-                if station.var_D_values[index] <= threshold_below_var_D:
+            if var_I >= thresholds_var_I['above']:
+                if station.var_D.specific_values[index] <= thresholds_var_D['below']:
                     contingency_table[2][0] += 1
-                if threshold_below_var_D < station.var_D_values[index] < threshold_above_var_D:
+                if thresholds_var_D['below'] < station.var_D.specific_values[index] < thresholds_var_D['above']:
                     contingency_table[2][1] += 1
-                if station.var_D_values[index] >= threshold_above_var_D:
+                if station.var_D.specific_values[index] >= thresholds_var_D['above']:
                     contingency_table[2][2] += 1
     else:
         # normal case
-        for index, var_I in enumerate(station.var_I_values):
-            if var_I < threshold_below_var_I:
-                if station.var_D_values[index] <= threshold_below_var_D:
+        for index, var_I in enumerate(station.var_I.specific_values):
+            if var_I < thresholds_var_I['below']:
+                if station.var_D.specific_values[index] <= thresholds_var_D['below']:
                     contingency_table[0][0] += 1
-                if threshold_below_var_D < station.var_D_values[index] < threshold_above_var_D:
+                if thresholds_var_D['below'] < station.var_D.specific_values[index] < thresholds_var_D['above']:
                     contingency_table[0][1] += 1
-                if station.var_D_values[index] >= threshold_above_var_D:
+                if station.var_D.specific_values[index] >= thresholds_var_D['above']:
                     contingency_table[0][2] += 1
-            if threshold_below_var_I <= var_I <= threshold_above_var_I:
-                if station.var_D_values[index] <= threshold_below_var_D:
+            if thresholds_var_I['below'] <= var_I <= thresholds_var_I['above']:
+                if station.var_D.specific_values[index] <= thresholds_var_D['below']:
                     contingency_table[1][0] += 1
-                if threshold_below_var_D < station.var_D_values[index] < threshold_above_var_D:
+                if thresholds_var_D['below'] < station.var_D.specific_values[index] < thresholds_var_D['above']:
                     contingency_table[1][1] += 1
-                if station.var_D_values[index] >= threshold_above_var_D:
+                if station.var_D.specific_values[index] >= thresholds_var_D['above']:
                     contingency_table[1][2] += 1
-            if var_I > threshold_above_var_I:
-                if station.var_D_values[index] <= threshold_below_var_D:
+            if var_I > thresholds_var_I['above']:
+                if station.var_D.specific_values[index] <= thresholds_var_D['below']:
                     contingency_table[2][0] += 1
-                if threshold_below_var_D < station.var_D_values[index] < threshold_above_var_D:
+                if thresholds_var_D['below'] < station.var_D.specific_values[index] < thresholds_var_D['above']:
                     contingency_table[2][1] += 1
-                if station.var_D_values[index] >= threshold_above_var_D:
+                if station.var_D.specific_values[index] >= thresholds_var_D['above']:
                     contingency_table[2][2] += 1
 
     # -------------------------------------------------------------------------
@@ -163,40 +213,40 @@ def get_contingency_table(station, lag, month, day=None):
     # because "nan"  character could not be calculate.
 
     # if threshold by below of independent variable is wrong
-    if float(sum_per_column_percent[0]) == 0 and not globals_vars.threshold_problem[0]:
+    if float(sum_per_column_percent[0]) == 0 and not env.globals_vars.threshold_problem[0]:
         console.msg(
             _(u"\n\n > WARNING: The thresholds selected '{0}' and '{1}'\n"
               u"   are not suitable for compound analysis of\n"
               u"   variable '{2}' with relation to '{3}' inside\n"
               u"   category '{4}'. Therefore, the graphics\n"
               u"   will not be created.")
-            .format(config_run.settings['threshold_below_var_I'], config_run.settings['threshold_above_var_I'],
-                station.var_D.type_series, station.var_I.type_series, config_run.settings['phen_below_label']), color='yellow')
-        globals_vars.threshold_problem[0] = True
+            .format(env.config_run.settings['threshold_below_var_I'], env.config_run.settings['threshold_above_var_I'],
+                station.var_D.type_series, station.var_I.type_series, env.config_run.settings['phen_below_label']), color='yellow')
+        env.globals_vars.threshold_problem[0] = True
 
     # if threshold by below or above calculating normal phenomenon of independent variable is wrong
-    if float(sum_per_column_percent[1]) == 0 and not globals_vars.threshold_problem[1]:
+    if float(sum_per_column_percent[1]) == 0 and not env.globals_vars.threshold_problem[1]:
         console.msg(
             _(u"\n\n > WARNING: The thresholds selected '{0}' and '{1}'\n"
               u"   are not suitable for compound analysis of\n"
               u"   variable '{2}' with relation to '{3}' inside\n"
               u"   category '{4}'. Therefore, the graphics\n"
               u"   will not be created.")
-            .format(config_run.settings['threshold_below_var_I'], config_run.settings['threshold_above_var_I'],
-                station.var_D.type_series, station.var_I.type_series, config_run.settings['phen_normal_label']), color='yellow')
-        globals_vars.threshold_problem[1] = True
+            .format(env.config_run.settings['threshold_below_var_I'], env.config_run.settings['threshold_above_var_I'],
+                station.var_D.type_series, station.var_I.type_series, env.config_run.settings['phen_normal_label']), color='yellow')
+        env.globals_vars.threshold_problem[1] = True
 
     # if threshold by above of independent variable is wrong
-    if float(sum_per_column_percent[2]) == 0 and not globals_vars.threshold_problem[2]:
+    if float(sum_per_column_percent[2]) == 0 and not env.globals_vars.threshold_problem[2]:
         console.msg(
             _(u"\n\n > WARNING: The thresholds selected '{0}' and '{1}'\n"
               u"   are not suitable for compound analysis of\n"
               u"   variable '{2}' with relation to '{3}' inside\n"
               u"   category '{4}'. Therefore, the graphics\n"
               u"   will not be created.")
-            .format(config_run.settings['threshold_below_var_I'], config_run.settings['threshold_above_var_I'],
-                station.var_D.type_series, station.var_I.type_series, config_run.settings['phen_above_label']), color='yellow')
-        globals_vars.threshold_problem[2] = True
+            .format(env.config_run.settings['threshold_below_var_I'], env.config_run.settings['threshold_above_var_I'],
+                station.var_D.type_series, station.var_I.type_series, env.config_run.settings['phen_above_label']), color='yellow')
+        env.globals_vars.threshold_problem[2] = True
 
     try:
         # not shows error if there are any problem with threshold
@@ -230,12 +280,12 @@ def contingency_table(station):
     contingencies_tables_percent = {}
     # defined if is first iteration
     station.first_iter = True
-    for lag in config_run.settings['lags']:
+    for lag in env.config_run.settings['lags']:
 
         tmp_month_list = []
         # all months in year 1->12
         for month in range(1, 13):
-            if globals_vars.STATE_OF_DATA in [1, 3]:
+            if env.globals_vars.STATE_OF_DATA in [1, 3]:
 
                 contingency_table,\
                 contingency_table_percent,\
@@ -247,7 +297,7 @@ def contingency_table(station):
                 if station.first_iter:
                     station.first_iter = False
 
-            if globals_vars.STATE_OF_DATA in [2, 4]:
+            if env.globals_vars.STATE_OF_DATA in [2, 4]:
                 tmp_day_list = []
                 for day in station.range_analysis_interval:
 
