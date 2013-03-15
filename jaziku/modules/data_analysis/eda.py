800cb2138893f7895484c08b99179bd2230d100b
Xavier Corredor Llano <xcorredorl@ideam.gov.co>
2013-03-15 01:01:04 -0500

    - Fixed the categorization the outliers with different mode calculation series - Fixes some typos

--- jaziku/modules/data_analysis/eda.py
+++ jaziku/modules/data_analysis/eda.py
@@ -1347,13 +1347,13 @@ def outliers(stations_list):
                 if env.var_I.is_monthly() and env.var_I.TYPE_SERIES in ['ONI1', 'ONI2', 'CAR']:
                     # take the first month (in this case, it is the mean of trimester)
                     values_var_I = values_var_I[0]
-                else:
-                    # get the mean or sum (based on mode_calculation_series_I) of all values of var I
-                    # in analysis interval in the corresponding period of outlier (var_D)
-                    if env.config_run.settings['mode_calculation_series_I'] == 'mean':
-                        value_var_I = array.mean(values_var_I)
-                    if env.config_run.settings['mode_calculation_series_I'] == 'accumulate':  # TODO v0.6.0: check (and accumulate??)
-                        value_var_I = sum(array.clean(values_var_I))
+
+                # get the mean or sum (based on mode_calculation_series_I) of all values of var I
+                # in analysis interval in the corresponding period of outlier (var_D)
+                if env.config_run.settings['mode_calculation_series_I'] == 'mean':
+                    value_var_I = array.mean(values_var_I)
+                if env.config_run.settings['mode_calculation_series_I'] == 'accumulate':  # TODO v0.6.0: check (and accumulate??)
+                    value_var_I = sum(array.clean(values_var_I))
 
                 # get categorize of phenomenon for the value_var_I
                 category_of_phenomenon = get_label_of_var_I_category(value_var_I, station)
