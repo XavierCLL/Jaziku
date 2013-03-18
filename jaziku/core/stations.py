c8ce199d85b3b5ddbc06a72ffdb28d34d77f5287
Xavier Corredor Llano <xcorredorl@ideam.gov.co>
2013-03-18 17:20:22 -0500

    - prepare data for all station function are independent when run data analysis and climate/forecast process

--- jaziku/core/stations.py
+++ jaziku/core/stations.py
@@ -27,106 +27,117 @@ from jaziku.core.input import validation
 from jaziku.utils import console
 
 
-def prepare_all_stations(stations_list):
+def prepare_all_stations(stations_list, prepare_data_for_data_analysis, prepare_data_for_climate_forecast):
 
-    print _("\n\n"
-            "##################### PREPARE ALL STATIONS #####################\n")
 
-    # Read vars
-    console.msg(_("Reading var D and var I of all stations ................. "), newline=False)
-    for station in stations_list:
-        station.var_D.read_data_from_file(station)
-        station.var_I.read_data_from_file(station)
-    console.msg(_("done"), color='green')
+    if prepare_data_for_data_analysis:
+        print _("\n\n"
+                "############ PREPARE ALL STATIONS FOR DATA ANALYSIS ############\n")
 
-    # show some information of variables
-    console.msg(_("   var_D ({0}):").format(env.var_D.TYPE_SERIES), newline=False)
-    if env.var_D.is_daily():
-        console.msg(_("has data daily").format(env.var_D.TYPE_SERIES), color='cyan')
-    else:
-        console.msg(_("has data monthly").format(env.var_D.TYPE_SERIES), color='cyan')
+    if prepare_data_for_climate_forecast:
+        print _("\n\n"
+                "####### PREPARE ALL STATIONS FOR CLIMATE/FORECAST PROCESS ######\n")
 
-    console.msg(_("   var_I ({0}):").format(env.var_I.TYPE_SERIES), newline=False)
-    if env.var_I.is_daily():
-        console.msg(_("has data daily").format(env.var_I.TYPE_SERIES), color='cyan')
-    else:
-        console.msg(_("has data monthly").format(env.var_I.TYPE_SERIES), color='cyan')
-
-    # show thresholds tu use
-    console.msg(_("Thresholds to use (for {0} categories):").format(env.config_run.settings['class_category_analysis']))
-
-    console.msg(_("   var_D ({0}):").format(env.var_D.TYPE_SERIES), newline=False)
-    if env.config_run.settings['thresholds_var_D'] == 'default':
-        thresholds_D = env.var_D.get_default_thresholds()
-    else:
-        thresholds_D = env.config_run.settings['thresholds_var_D']
-    if isinstance(thresholds_D, list):
-        console.msg(' | '.join([str(thr) for thr in thresholds_D]), color='cyan')
-
-    console.msg(_("   var_I ({0}):").format(env.var_I.TYPE_SERIES), newline=False)
-    if env.config_run.settings['thresholds_var_I'] == 'default':
-        thresholds_I = env.var_I.get_default_thresholds()
-    else:
-        thresholds_I = env.config_run.settings['thresholds_var_I']
-    if isinstance(thresholds_I, list):
-        console.msg(' | '.join([str(thr) for thr in thresholds_I]), color='cyan')
-
-    # common and process period
-    console.msg(_("Calculate common and process period for each stations ... "), newline=False)
-    for station in stations_list:
-        station.calculate_common_and_process_period()
-    console.msg(_("done"), color='green')
-
-    # data, date and null
-    console.msg(_("Prepare data, date and null in process period ........... "), newline=False)
-    for station in stations_list:
-        station.var_D.data_and_null_in_process_period(station)
-        station.var_I.data_and_null_in_process_period(station)
-    console.msg(_("done"), color='green')
-
-    if env.config_run.settings['consistent_data']:
-        console.msg(_("Check if the data are consistent for var_D and var_I:"))
+    if prepare_data_for_data_analysis or \
+      (prepare_data_for_climate_forecast and not env.config_run.settings['data_analysis']):
+        # Read vars
+        console.msg(_("Reading var D and var I of all stations ................. "), newline=False)
         for station in stations_list:
-            validation.check_consistent_data(station)
+            station.var_D.read_data_from_file(station)
+            station.var_I.read_data_from_file(station)
+        console.msg(_("done"), color='green')
 
-    # state of data
-    console.msg(_("Set state of data ....................................... "), newline=False)
-    env.globals_vars.STATE_OF_DATA = analysis_interval.get_state_of_data()
-    console.msg(_("done"), color='green')
+        # show some information of variables
+        console.msg(_("   var_D ({0}):").format(env.var_D.TYPE_SERIES), newline=False)
+        if env.var_D.is_daily():
+            console.msg(_("has data daily").format(env.var_D.TYPE_SERIES), color='cyan')
+        else:
+            console.msg(_("has data monthly").format(env.var_D.TYPE_SERIES), color='cyan')
 
-    # analysis interval
-    console.msg(_("Check analysis interval ................................. "), newline=False)
-    analysis_interval.check_analysis_interval()
-    console.msg(_("done"), color='green')
+        console.msg(_("   var_I ({0}):").format(env.var_I.TYPE_SERIES), newline=False)
+        if env.var_I.is_daily():
+            console.msg(_("has data daily").format(env.var_I.TYPE_SERIES), color='cyan')
+        else:
+            console.msg(_("has data monthly").format(env.var_I.TYPE_SERIES), color='cyan')
 
-    # when var D is monthly and var I is daily, only can process with data monthly,
-    # then, Jaziku need convert var I of all stations to data monthly
-    console.msg(_("Adjust data of all variables if is needed:"))
-    is_data_adjusted = analysis_interval.adjust_data_of_variables(stations_list)
-    if not is_data_adjusted:
-        console.msg(_("   data not need to be adjusted"), color='cyan')
+        # show thresholds tu use
+        console.msg(_("Thresholds to use (for {0} categories):").format(env.config_run.settings['class_category_analysis']))
 
-    if is_data_adjusted:
+        console.msg(_("   var_D ({0}):").format(env.var_D.TYPE_SERIES), newline=False)
+        if env.config_run.settings['thresholds_var_D'] == 'default':
+            thresholds_D = env.var_D.get_default_thresholds()
+        else:
+            thresholds_D = env.config_run.settings['thresholds_var_D']
+        if isinstance(thresholds_D, list):
+            console.msg(' | '.join([str(thr) for thr in thresholds_D]), color='cyan')
+
+        console.msg(_("   var_I ({0}):").format(env.var_I.TYPE_SERIES), newline=False)
+        if env.config_run.settings['thresholds_var_I'] == 'default':
+            thresholds_I = env.var_I.get_default_thresholds()
+        else:
+            thresholds_I = env.config_run.settings['thresholds_var_I']
+        if isinstance(thresholds_I, list):
+            console.msg(' | '.join([str(thr) for thr in thresholds_I]), color='cyan')
+
         # common and process period
-        console.msg(_("Re-calculate common and process period for each stations  "), newline=False)
+        console.msg(_("Calculate common and process period for each stations ... "), newline=False)
         for station in stations_list:
             station.calculate_common_and_process_period()
         console.msg(_("done"), color='green')
 
         # data, date and null
-        console.msg(_("Re-prepare data, date and null in process period ........ "), newline=False)
+        console.msg(_("Prepare data, date and null in process period ........... "), newline=False)
         for station in stations_list:
             station.var_D.data_and_null_in_process_period(station)
             station.var_I.data_and_null_in_process_period(station)
         console.msg(_("done"), color='green')
 
-    # statistics for data analysis
-    if env.config_run.settings['data_analysis']:
-        console.msg(_("Statistics of data for data analysis module ............. "), newline=False)
-        for station in stations_list:
-            station.var_D.do_some_statistic_of_data()
-            station.var_I.do_some_statistic_of_data()
+        if env.config_run.settings['consistent_data']:
+            console.msg(_("Check if the data are consistent for var_D and var_I:"))
+            for station in stations_list:
+                validation.check_consistent_data(station)
+
+        # state of data
+        console.msg(_("Set state of data ....................................... "), newline=False)
+        env.globals_vars.STATE_OF_DATA = analysis_interval.get_state_of_data()
         console.msg(_("done"), color='green')
+
+        # analysis interval
+        console.msg(_("Check analysis interval ................................. "), newline=False)
+        analysis_interval.check_analysis_interval()
+        console.msg(_("done"), color='green')
+
+
+    if prepare_data_for_climate_forecast:
+        # when var D is monthly and var I is daily, only can process with data monthly,
+        # then, Jaziku need convert var I of all stations to data monthly
+        console.msg(_("Adjust data of all variables if is needed:"))
+        is_data_adjusted = analysis_interval.adjust_data_of_variables(stations_list)
+        if not is_data_adjusted:
+            console.msg(_("   data not need to be adjusted"), color='cyan')
+
+        if is_data_adjusted:
+            # common and process period
+            console.msg(_("Re-calculate common and process period for each stations  "), newline=False)
+            for station in stations_list:
+                station.calculate_common_and_process_period()
+            console.msg(_("done"), color='green')
+
+            # data, date and null
+            console.msg(_("Re-prepare data, date and null in process period ........ "), newline=False)
+            for station in stations_list:
+                station.var_D.data_and_null_in_process_period(station)
+                station.var_I.data_and_null_in_process_period(station)
+            console.msg(_("done"), color='green')
+
+    if prepare_data_for_data_analysis:
+        # statistics for data analysis
+        if env.config_run.settings['data_analysis']:
+            console.msg(_("Statistics of data for data analysis module ............. "), newline=False)
+            for station in stations_list:
+                station.var_D.do_some_statistic_of_data()
+                station.var_I.do_some_statistic_of_data()
+            console.msg(_("done"), color='green')
 
     console.msg('')
     console.msg(gettext.ngettext(
