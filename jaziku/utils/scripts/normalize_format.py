f711457a03ed549c7411f32ff492173d75310678
Xavier Corredor Llano <xcorredorl@ideam.gov.co>
2013-11-04 15:58:33 -0500

    - Adjust and some fixes in 'normalize_format' , convert the old null and show messages in the console - Update the incoming plans

--- jaziku/utils/scripts/normalize_format.py
+++ jaziku/utils/scripts/normalize_format.py
@@ -31,6 +31,8 @@
 # Attention!: this script overwrite the same file, please make backup first
 
 import sys
+import re
+from shutil import copyfile
 from datetime import date
 from dateutil.relativedelta import relativedelta
 from jaziku.utils import output 
@@ -39,7 +41,6 @@ from jaziku.utils import output
 def external_run():
     all_input_files = sys.argv[1::]
     for time_series_file in all_input_files:
-        print 'processing file: ' + time_series_file
         main(time_series_file)
 
 def get_int_month(month):
@@ -60,28 +61,31 @@ def get_char_month(month, type_of_month):
     if type_of_month == 'bimonthly':
         bim_char = {1:'jf', 2: 'fm', 3: 'ma',4: 'am', 5: 'mj', 6: 'jj', 7: 'ja', 8: 'as', 9: 'so', 10: 'on', 11: 'nd', 12: 'dj'}
         return bim_char[month]
-    if type_of_month == 'trimester':
+    if type_of_month == 'trimonthly':
         trim_char = {1:'jfm', 2: 'fma', 3: 'mam',4: 'amj', 5: 'mjj', 6: 'jja', 7: 'jas', 8: 'aso', 9: 'son', 10: 'ond', 11: 'ndj', 12: 'djf'}
         return trim_char[month]
 
 def main(time_series_file):
 
+    output.console.msg('processing file: ' + time_series_file, color='cyan')
+
+    output.console.msg('  making backup in ' + time_series_file+'~')
+    copyfile(time_series_file, time_series_file+'~')
+
     input_file = open(time_series_file, 'rU')
-
     lines = input_file.readlines()
-
     input_file.close()
 
     output_file = open(time_series_file, 'w')
     old_date_value = False
 
-    for line in lines:
+    for num_line, line in enumerate(lines):
 
         if not line or not line[0].strip() or line == []:
             continue
 
         is_daily = False
-        is_monthly = False
+        is_n_monthly = False
         type_of_month = False
 
         # normalize characters and divisions, and clean
@@ -95,52 +99,71 @@ def main(time_series_file):
         line = line.replace('NAN','nan')
         line = line.strip()
 
-        year = line.split(' ')[0].split('-')[0]
-        month = line.split(' ')[0].split('-')[1]
-        try:
+        year = re.sub(r'[^\w]', '', line.split(' ')[0].split('-')[0])
+        month = re.sub(r'[^\w]', '', line.split(' ')[0].split('-')[1])
+
+        if month.isdigit():
             month = int(month)
             type_of_month = 'monthly'
-        except:
-            if isinstance(month, str) and len(month) in [2,3]:
-                month = month.lower()
-                if len(month) == 2:
-                    type_of_month = 'bimonthly'
-                if len(month) == 3:
-                    type_of_month = 'trimester'
+        elif len(month) in [2,3]:
+            month = month.lower()
+            if len(month) == 2:
+                type_of_month = 'bimonthly'
+            if len(month) == 3:
+                type_of_month = 'trimonthly'
+        else:
+            output.console.msg_error('unknown month format in line '+str(num_line+1),wait_value=False)
 
         if len(line.split(' ')[0].split('-')) == 3:
-            day = line.split(' ')[0].split('-')[2]
+            day = re.sub(r'[^\w]', '', line.split(' ')[0].split('-')[2])
             day = output.fix_zeros(int(day))
             is_daily = True
         else:
             day = 1
-            is_monthly = True
+            is_n_monthly = True
 
         # detect if value is not empty, else put 'nan' as value
         if len(line.split(' ')) >= 2 and line.split(' ')[1].strip() != '':
             value = line.split(' ')[1]
+            # transform the old null to new valid null "nan"
+            if value.startswith(('-99999','99999')) and int(float(value)) in [-99999,99999]:
+                value = 'nan'
         else:
             value = 'nan'
+            if is_n_monthly:
+                output.console.msg('  filling missing values for date {0}-{1} in line {2}'
+                    .format(year, month, num_line+1), color='yellow')
+            if is_daily:
+                output.console.msg('  filling missing values for date {0}-{1}-{2} in line {3}'
+                    .format(year, get_char_month(month, type_of_month), day, num_line+1), color='yellow')
 
         date_value = date(int(year),get_int_month(month),int(day))
 
         # fill the empty months or days with nan if not exists
         if old_date_value is not False:
-            if is_monthly:
+            if is_n_monthly:
                 while date_value > old_date_value + relativedelta(months=1):
                     old_date_value = old_date_value + relativedelta(months=1)
                     output_file.write(str(old_date_value.year)+'-'+get_char_month(old_date_value.month, type_of_month)+' '+'nan'+'\n')
+
+                    output.console.msg('  filling missing dates with nan for {0}-{1} in line {2}'
+                        .format(old_date_value.year, get_char_month(old_date_value.month, type_of_month), num_line+1), color='yellow')
+
+
             if is_daily:
                 while date_value > old_date_value + relativedelta(days=1):
                     old_date_value = old_date_value + relativedelta(days=1)
                     output_file.write(str(old_date_value.year)+'-'+output.fix_zeros(old_date_value.month)+'-'+output.fix_zeros(old_date_value.day)+' '+'nan'+'\n')
+
+                    output.console.msg('  filling missing dates with nan for {0}-{1}-{2} in line {3}'
+                            .format(old_date_value.year, get_char_month(old_date_value.month, type_of_month), old_date_value.day, num_line+1), color='yellow')
 
         old_date_value = date_value
 
         if type_of_month == 'monthly':
             month = output.fix_zeros(int(month))
 
-        if is_monthly:
+        if is_n_monthly:
             output_file.write(year+'-'+str(month)+' '+str(value)+'\n')
         if is_daily:
             output_file.write(year+'-'+str(month)+'-'+str(day)+' '+str(value)+'\n')
