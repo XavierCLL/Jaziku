4e913295d8e52a8b0450779c8f75d3e852d17fcc
Xavier Corredor Llano <xcorredorl@ideam.gov.co>
2014-10-03 17:32:13 -0500

    - Added the period to process in all maps in tob-center of the graphics, and put the analog year label in bottom left location into the maps only if this is enable.

--- jaziku/data/maps/shapes/Colombia/Colombia/ncl_script_probabilistic_map.py
+++ jaziku/data/maps/shapes/Colombia/Colombia/ncl_script_probabilistic_map.py
@@ -42,6 +42,7 @@ def code(map_properties):
         'colormap': map_properties.colormap,
         'name': '''"{0}"'''.format(map_properties.name),
         'subtitle': map_properties.subtitle,
+        'text_bottom_left': map_properties.text_bottom_left,
         'units': map_properties.units
     }
 
@@ -379,7 +380,21 @@ begin
     annoid1 = gsn_add_annotation(map,lbid,amres)   ; attach legend to plot
 
   end if
+  ;*****************************************
 
+  ;*****************************************
+  ; plotting text BottomLeft
+  txres                    = True
+  txres@lgAutoManage       = True
+  txres@txFontHeightF = 0.0085
+  txt_bl = gsn_create_text(wks, {text_bottom_left}, txres)
+
+  amres_bl = True
+  amres_bl@amParallelPosF   = -0.49   ; This is the right edge of the plot.
+  amres_bl@amOrthogonalPosF = 0.495    ; This is the bottom edge of the plot.
+  amres_bl@amJust           = "BottomLeft"
+
+  annoid1 = gsn_add_annotation(map, txt_bl, amres_bl)
   ;*****************************************
 
   ;## add subtitles
